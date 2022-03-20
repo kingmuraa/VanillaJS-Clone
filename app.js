e11a487c9fa9113c275c654699b5724cb35f40f0
Hyunseok Kim <75778561+kingmuraa@users.noreply.github.com>
2022-03-20 21:27:19 +0900

    220320

--- app.js
+++ app.js
@@ -1,10 +1,41 @@
-alert("hi");
+const h1 = document.querySelector("div.hello:first-child h1");
 
-let a = 5;
-let b = 2;
-let myName = "hyunseok";
+function handleTiltleClick(){
+    h1.style.color = "blue";
+}
 
-console.log(a + b);
-console.log(a * b);
-console.log(a / b);
-console.log("hello " + myName);
+function handleMouseEnter(){
+    h1.innerText = "Mouse is here !";
+}
+
+function handleMouseLeave(){
+    h1.innerText = "Mouse is gone !";
+}
+
+function handleWindowResize(){
+    document.body.style.backgroundColor = "tomato";
+}
+
+function handleWindowCopy(){
+    alert("copier!");
+}
+
+function handleWindowOffline(){
+    alert("SOS no WIFI");
+}
+
+function handleWindowOnline(){
+    alert("All GOOOD");
+}
+
+// hi.onclick = handleTiltleClick;
+// hi.onmouseenter = handleMouseEnter;
+// hi.onmouseleave = handleMouseLeave;
+h1.addEventListener("click", handleTiltleClick);
+h1.addEventListener("mouseenter", handleMouseEnter);
+h1.addEventListener("mouseleave", handleMouseLeave);
+
+window.addEventListener("resize",handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline", handleWindowOffline);
+window.addEventListener("online", handleWindowOnline);
