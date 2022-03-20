469bf73c3a4fab66778d6920417c10f064464897
Hyunseok Kim <75778561+kingmuraa@users.noreply.github.com>
2022-03-20 22:39:03 +0900

    220320

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTiltleClick(){
-    const clickedClass = "clicked";
-    if(h1.className === clickedClass) {
-        h1.className = "";
-    }else {
-        h1.className = clickedClass;
-    }
+    
+    h1.classList.toggle("clicked");
+
+    // if(h1.classList.contains(clickedClass)) {
+    //     h1.classList.remove(clickedClass);
+    // }else {
+    //     h1.classList.add(clickedClass);
+    // }
 }
 
 h1.addEventListener("click", handleTiltleClick);
