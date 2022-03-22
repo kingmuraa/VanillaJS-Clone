1accdf408bd7138cd83c3cc323a9d364ae04eed0
Hyunseok Kim <75778561+kingmuraa@users.noreply.github.com>
2022-03-23 06:04:45 +0900

    220323

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+const HIDDEN_CLASSNAME = "hidden";
 
-const link = document.querySelector("a");
 
 function onLoginSubmit(event) {
     event.preventDefault(); 
-    console.log(loginInput.value);
+    const username = loginInput.value;
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    console.log(username);
+    // greeting.innerText = "Hello " + username;
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
-
-function handleLinkClick(event) {
-    event.preventDefault(); 
-    console.dir(event);
-}
-
+ 
 loginForm.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click", handleLinkClick);
