f310e12e41d4efdc51c840edd2a8a50584240718
Hyunseok Kim <75778561+kingmuraa@users.noreply.github.com>
2022-03-22 23:47:33 +0900

    220322

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 
+const link = document.querySelector("a");
+
 function onLoginSubmit(event) {
-    event.preventDefault();
-    // const username = loginInput.value;
-    console.log(event);
+    event.preventDefault(); 
+    console.log(loginInput.value);
+}
+
+function handleLinkClick(event) {
+    event.preventDefault(); 
+    console.dir(event);
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
-
-
+link.addEventListener("click", handleLinkClick);
