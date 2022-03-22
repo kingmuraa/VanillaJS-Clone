07ab234e1e0be0aefb6ac5d47f43425289efdfa2
Hyunseok Kim <75778561+kingmuraa@users.noreply.github.com>
2022-03-22 23:00:14 +0900

    220322

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
+const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
+
+function onLoginSubmit(event) {
+    event.preventDefault();
+    // const username = loginInput.value;
+    console.log(event);
+}
+
+loginForm.addEventListener("submit", onLoginSubmit);
 
 
-function onLoginBtnClick(){
-    const username = loginInput.value;
-    if (username ==="") {
-        alert("Please write your name");
-         
-    } else if(username.length > 15) {
-        alert("Your name is too long.");
-    }
-}   
-loginButton.addEventListener("click", onLoginBtnClick);
