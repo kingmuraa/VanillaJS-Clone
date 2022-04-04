276a323a69b27635d7f85d84618f3021315b4638
Hyunseok Kim <75778561+kingmuraa@users.noreply.github.com>
2022-04-04 21:18:21 +0900

    220404

---
+++ js/weather.js
@@ -0,0 +1,10 @@
+function onGeoO(position) {
+    const lat = position.coords.latitude;
+    const lng = position.coords.longitude;
+    console.log("You live it", lat, lng);
+}
+function onGeoError() { 
+    alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoO, onGeoError);
