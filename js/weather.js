0082b03dd0cb4d5bb43fa546137ccfb859edc09a
Hyunseok Kim <75778561+kingmuraa@users.noreply.github.com>
2022-04-04 22:15:03 +0900

    220404

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,17 @@
+const API_KEY = "6b181c6f95e703e750e3ee86cd893687";
+
 function onGeoO(position) {
     const lat = position.coords.latitude;
     const lng = position.coords.longitude;
-    console.log("You live it", lat, lng);
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}`;
+    fetch(url)
+    .then((response) => response.json())
+    .then(data => {
+        const weather = document.querySelector("#weather span:first-child");
+        const city = document.querySelector("#weather span:last-child");
+        city.innerText = data.name;
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`
+    });
 }
 function onGeoError() { 
     alert("Can't find you. No weather for you.");
