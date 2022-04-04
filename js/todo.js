c342d6c098c1022e01f8c5fb91d3d7ffaf983c7b
Hyunseok Kim <75778561+kingmuraa@users.noreply.github.com>
2022-04-04 20:22:30 +0900

    220404

--- js/todo.js
+++ js/todo.js
@@ -12,13 +12,14 @@ function saveToDos() {
 
 function deleteToDo(event) {
     const li = event.target.parentElement;
-    li.remove();
+    li.remove(); 
 }
 
 function paintToDo(newTodo) {
     const li = document.createElement("li");
+    li.id = newTodo.id;
     const span = document.createElement("span");
-    span.innerText = newTodo;
+    span.innerText = newTodo.text;
     const button = document.createElement("button");
     button.innerText = "❌";
     button.addEventListener("click", deleteToDo);
@@ -31,8 +32,12 @@ function handleToDoSubmit(event) {
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = "";
-    toDos.push(newTodo);
-    paintToDo(newTodo);
+    const newTodoObj = {
+        text:newTodo,
+        id: Date.now(),
+    };
+    toDos.push(newTodoObj);
+    paintToDo(newTodoObj);
     saveToDos();
 }
 
